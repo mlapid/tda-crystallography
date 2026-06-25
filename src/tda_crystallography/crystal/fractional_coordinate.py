9008df73303fda7abb1f21a67df88c516207bcbf
mlapid <marcus.lapid@gmail.com>
2026-06-25 22:58:33 +0200

    Crystal - Updated Fractional Coordinate

--- src/tda_crystallography/crystal/fractional_coordinate.py
+++ src/tda_crystallography/crystal/fractional_coordinate.py
@@ -1,5 +1,6 @@
 from typing import Annotated
 
+import numpy as np
 from pydantic.config import ConfigDict
 from pydantic import BaseModel, Field, field_validator
 
@@ -27,11 +28,17 @@ class FractionalCoordinate(BaseModel):
         return hash((self.__class__.__name__, self.x, self.y, self.z))
 
     def __str__(self) -> str:
-        return f'{self.__class__.__name__}(x={self.x}, y={self.y}, z={self.z})'
+        return (
+            f'{self.__class__.__name__}'
+            f'(x={self.x:.5f}, y={self.y:.5f}, z={self.z:.5f})'
+        )
     
     def __repr__(self) -> str:
-        return self.__str__()
+        return (
+            f'{self.__class__.__name__}'
+            f'(x={self.x}, y={self.y}, z={self.z})'
+        )
     
     def orthogonalise(self, unit_cell: UnitCell) -> PositionalCoordinate:
-        x, y, z = unit_cell.orthogonalisation_matrix @ [self.x, self.y, self.z]
-        return PositionalCoordinate(x=float(x), y=float(y), z=float(z))
+        x, y, z = unit_cell.orthogonalisation_matrix @ np.array([self.x, self.y, self.z], dtype=float)
+        return PositionalCoordinate(x=x, y=y, z=z)
